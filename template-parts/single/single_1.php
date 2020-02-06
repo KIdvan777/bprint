950dbdee5722b5294c89e5ea7e3e833f9d8d7770
kidvan777 <haskellisp@gmail.com>
2020-02-06 22:49:29 +0300

    Next

---
+++ template-parts/single/single_1.php
@@ -0,0 +1,138 @@
+<section class="single_1_section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="single_1_images">
+                    <div class="single_1_images_main">
+
+                    </div>
+                    <div class="single_1_images_gallery">
+
+                    </div>
+                </div>
+                <div class="single_1_content">
+                    <h2>Autumn Winter fashion Europian</h2>
+                    <span>Category</span>
+                    <span>Condition</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <div class="single_1_content_attr">
+                        <form class="" action="index.html" method="post">
+                            <label for="">Size:
+                                <input type="text" name="" value="">
+                            </label>
+                            <label for="">Color:
+                                <input type="text" name="" value="">
+                            </label>
+                        </form>
+                    </div>
+                    <span class="top_hits_1_item_content_price_old_col">$129.86</span>
+                    <span class="top_hits_1_item_content_price_col">$89.86</span>
+                    <button type="button" name="button">Buy it Now</button>
+                    <h3>Availiable until Monday!</h3>
+                    <span>We reached our goal! You can keep byying untill the campaign ends!</span>
+                    <div class="">
+                        48 20 58 00
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="single_1_heading">
+                    <ul>
+                        <li>DESCRIPTION</li>
+                        <li>REWIEWS</li>
+                        <li>CUSTOM TABS</li>
+                    </ul>
+                    <div class="single_1_description">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div class="single_1_reviews">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div class="single_1_custom_tabs">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                </div>
+            </div>
+            <section class="top_hits_1_section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 wrapper">
+                            <div class="section_heading flex center-horizontal">
+                                <h5>BEST SELLER THIS WEEK</h5>
+                            </div>
+                            <div class="top_hits_1_line">
+
+                            </div>
+                            <ul class="top_hits_1_items col-md-12 flex">
+                                <li class="top_hits_2_item col-md-3">
+                                    <a href="#" class="flex">
+                                        <div class="top_hits_2_item_img">
+                                            <img src="<?php echo get_template_directory_uri() . '/assets/img/Men-t-shirt-2.png'?>"/>
+                                        </div>
+                                        <div class="top_hits_2_item_content flex center-horizontal center-vertical">
+                                            <h2>Funny St.Patrick`s Day Beer T-Shirt</h2>
+                                            <p>By Funny Patrick`s Day</p>
+                                            <span class="top_hits_1_item_content_price_old">$129.86</span>
+                                            <span class="top_hits_1_item_content_price">$89.86</span>
+                                        </div>
+                                        <div class="top_hits_1_item_sale">
+                                            -35%
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="top_hits_2_item col-md-3">
+                                    <a href="#" class="flex">
+                                        <div class="top_hits_2_item_img">
+                                            <img src="<?php echo get_template_directory_uri() . '/assets/img/Men-t-shirt-2.png'?>"/>
+                                        </div>
+                                        <div class="top_hits_2_item_content flex center-horizontal center-vertical">
+                                            <h2>Funny St.Patrick`s Day Beer T-Shirt</h2>
+                                            <p>By Funny Patrick`s Day</p>
+                                            <span class="top_hits_1_item_content_price_old">$129.86</span>
+                                            <span class="top_hits_1_item_content_price">$89.86</span>
+                                        </div>
+                                        <div class="top_hits_1_item_sale">
+                                            -35%
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="top_hits_2_item col-md-3">
+                                    <a href="#" class="flex">
+                                        <div class="top_hits_2_item_img">
+                                            <img src="<?php echo get_template_directory_uri() . '/assets/img/Men-t-shirt-2.png'?>"/>
+                                        </div>
+                                        <div class="top_hits_2_item_content flex center-horizontal center-vertical">
+                                            <h2>Funny St.Patrick`s Day Beer T-Shirt</h2>
+                                            <p>By Funny Patrick`s Day</p>
+                                            <span class="top_hits_1_item_content_price_old">$129.86</span>
+                                            <span class="top_hits_1_item_content_price">$89.86</span>
+                                        </div>
+                                        <div class="top_hits_1_item_sale">
+                                            -35%
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="top_hits_2_item col-md-3">
+                                    <a href="#" class="flex">
+                                        <div class="top_hits_2_item_img">
+                                            <img src="<?php echo get_template_directory_uri() . '/assets/img/Men-t-shirt-2.png'?>"/>
+                                        </div>
+                                        <div class="top_hits_2_item_content flex center-horizontal center-vertical">
+                                            <h2>Funny St.Patrick`s Day Beer T-Shirt</h2>
+                                            <p>By Funny Patrick`s Day</p>
+                                            <span class="top_hits_1_item_content_price_old">$129.86</span>
+                                            <span class="top_hits_1_item_content_price">$89.86</span>
+                                        </div>
+                                        <div class="top_hits_1_item_sale">
+                                            -35%
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</section>
